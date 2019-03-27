6e5f2722bd780a8a43756d00e2d5a7824bf1823b
Ahmed Hosny <i.ahmedhosny@gmail.com>
2019-03-27 12:07:19 -0400

    changes backend to mxnet

--- contrib_src/inference.py
+++ contrib_src/inference.py
@@ -1,8 +1,10 @@
-import onnx
-import caffe2.python.onnx.backend
 import json
 from processing import ImageProcessor
 from modelhublib.model import ModelBase
+import mxnet as mx
+import numpy as np
+from collections import namedtuple
+from mxnet.contrib.onnx.onnx2mx.import_model import import_model
 
 
 class Model(ModelBase):
@@ -12,17 +14,25 @@ class Model(ModelBase):
         config = json.load(open("model/config.json"))
         # get the image processor
         self._imageProcessor = ImageProcessor(config)
-        # load the DL model
-        self._model = onnx.load('model/model.onnx')
-    
+        # get context - cpu
+        ctx = mx.cpu()
+        sym, arg_params, aux_params = import_model('model/model.onnx')
+        # Load module
+        mod = mx.mod.Module(symbol=sym, context=ctx, label_names=None)
+        mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))],
+                 label_shapes=mod._label_shapes)
+        mod.set_params(arg_params, aux_params, allow_missing=True, allow_extra=True)
+
+        self._model = mod
 
     def infer(self, input):
         # load preprocessed input
         inputAsNpArr = self._imageProcessor.loadAndPreprocess(input)
-        # Run inference with caffe2
-        results = caffe2.python.onnx.backend.run_model(self._model, [inputAsNpArr])
+        # Run inference with mxnet
+        batch = namedtuple('Batch', ['data'])
+        self._model.forward(batch([inputAsNpArr]))
+        # Take softmax to generate probabilities
+        scores = mx.ndarray.softmax(self._model.get_outputs()[0][0]).asnumpy()
         # postprocess results into output
-        output = self._imageProcessor.computeOutput(results)
+        output = self._imageProcessor.computeOutput(scores)
         return output
-        
-
--- contrib_src/processing.py
+++ contrib_src/processing.py
@@ -1,39 +1,37 @@
+import numpy as np
+import os
+import mxnet as mx
 from modelhublib.processor import ImageProcessorBase
 import PIL
 import SimpleITK
 import numpy as np
 import json
-
+from mxnet.gluon.data.vision import transforms
 
 class ImageProcessor(ImageProcessorBase):
 
     def _preprocessBeforeConversionToNumpy(self, image):
         if isinstance(image, PIL.Image.Image):
-            image = image.resize((224,224), resample = PIL.Image.LANCZOS)
-        elif isinstance(image, SimpleITK.Image):
-            newSize = [224, 224]
-            referenceImage = SimpleITK.Image(newSize, image.GetPixelIDValue())
-            referenceImage.SetOrigin(image.GetOrigin())
-            referenceImage.SetDirection(image.GetDirection())
-            referenceImage.SetSpacing([sz*spc/nsz for nsz,sz,spc in zip(newSize,
-                                                                        image.GetSize(),
-                                                                        image.GetSpacing())])
-            image = SimpleITK.Resample(image, referenceImage)
+            image = np.array(image).astype(np.float32)
+            if len(image.shape) > 2:
+                image = image[:,:,0:3]
+            else:
+                image = np.stack((image,)*3, axis=-1)
+            arr = mx.nd.array(image)
+            transform_fn = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+            arr = transform_fn(arr)
+            arr = arr.expand_dims(axis=0)
+            return arr.asnumpy()
         else:
             raise IOError("Image Type not supported for preprocessing.")
-        return image
 
     def _preprocessAfterConversionToNumpy(self, npArr):
-        if npArr.shape[1] > 3:
-            npArr = npArr[:,0:3,:,:]
-        elif npArr.shape[1] < 3:
-            npArr = npArr[:,[0],:,:]
-            npArr = np.concatenate((npArr, npArr[:,[0],:,:]), axis = 1)
-            npArr = np.concatenate((npArr, npArr[:,[0],:,:]), axis = 1)
-        npArr = npArr.reshape(3, 224, 224)
-        npArr = self._preprocess(npArr)
-        npArr = npArr.reshape(1, 3, 224, 224)
-        return npArr
+        return mx.nd.array(npArr)
 
     def computeOutput(self, inferenceResults):
         probs = np.squeeze(np.asarray(inferenceResults))
@@ -45,12 +43,3 @@ class ImageProcessor(ImageProcessorBase):
                     'probability': float(probs[i])}
             result.append(obj)
         return result
-
-    def _preprocess(self, img_data):
-        mean_vec = np.array([0.485, 0.456, 0.406])
-        stddev_vec = np.array([0.229, 0.224, 0.225])
-        norm_img_data = np.zeros(img_data.shape).astype('float32')
-        for i in range(img_data.shape[0]):
-             # for each pixel in each channel, divide the value by 255 to get value between [0, 1] and then normalize
-            norm_img_data[i,:,:] = (img_data[i,:,:]/255 - mean_vec[i]) / stddev_vec[i]
-        return norm_img_data
